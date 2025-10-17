c4ebdee86935c4167f3140543f27e078d88d964c
Albert Qu <albertqu1@outlook.com>
2025-10-17 12:33:00 -0600

    chart test 1

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 import streamlit as st 
+import scipy.stats as stats 
+import time
 
 st.header('Flip a Coin 🪙')
 
-number_of_trials= st.slider('attemps', 1,1000, 100)
+chart= st.line_chart([.5])
+
+def toss_coin(n):
+    trial_outcomes= stats.bernoulli.rvs(p=.5, size= n)
+    mean = None
+    outcome_0_count = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_0_count +=1
+        if r == 1:
+            outcome_1_count +=1
+        mean = outcome_1_count / outcome_0_count
+        chart.add_rows([mean])
+        time.sleep(.05)
+    return mean
+
+number_of_trials= st.slider('attemps', 1,1000, 50, 1)
 start_button= st.button('Start')
 
 if start_button:
