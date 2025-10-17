9837d0c2c290a382ca36b01b93fbaa73a36604cd
Albert Qu <albertqu1@outlook.com>
2025-10-17 11:21:04 -0600

    slider fix

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.header('Flip a Coin 🪙')
 
-number_of_trials= st.slider('attemps', 1,10,25,50,100,500,1000)
+number_of_trials= st.slider('attemps', 1,1000, 100)
 start_button= st.button('Start')
 
 if start_button:
