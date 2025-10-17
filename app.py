adcb52164001c806d47eeed406157e4ec67b249a
Albert Qu <albertqu1@outlook.com>
2025-10-17 11:16:38 -0600

    button fix(?)

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ st.header('Flip a Coin 🪙')
 
 number_of_trials= st.slider('attemps', 1,10,25,50,100,500,1000)
 start_button= st.button('Start')
+
+if start_button:
+    st.write(f'test running with {number_of_trials} attemps')
+
 st.markdown('Under Construction  \nplease return later')
